052de32f5bb72b988488f919b7f903df4a5e77b5
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-06-18 01:01:45 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from "react";
+import {createPortal} from "react-dom";
 import logo from './logo.svg';
 import './App.css';
+import BasicTable from "./Table/Table";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+            <BasicTable/>
+
+        </>
+    );
 }
 
+
 export default App;
+
+
